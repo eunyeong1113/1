bed21d353b52162d74dc8c3f83ffbac9f2a97ab0
eunyeong1113 <eylee1113@naver.com>
2020-01-20 23:38:11 +0900

    PY

---
+++ 2020.01.20/1_12.py
@@ -0,0 +1,14 @@
+days=("Mon","Tue","Wed","Thu","Fri")
+
+for x in days:
+    if x is "Wed":
+        break
+    else:
+        print(x)
+    
+
+for a in [1,2,3,4,5]:
+    print(a)
+    
+for letter in "nicolas":
+    print(letter)
---
+++ 2020.01.20/1_13.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.ceil(1.2))
+print(math.fabs(-1.2))
+
+from math import ceil
+print(ceil(2.2))
+
+from math import fsum as sexy_sum
+
+print(sexy_sum([1,2,3,4,5,6,7]))
+
+from calculator import plus,minus
+print(plus(3,5),minus(3,5))
